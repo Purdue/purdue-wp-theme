46c3b554fe69147208d99b460c43f107e1bab8d3
jyoung295 <young295@purdue.edu>
2020-07-22 11:33:25 -0400

    add logic to print title-nav block above page title

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -9,19 +9,52 @@
  */
 ?>
 
-<?php if (!is_front_page() && !is_page() && !has_block('purdue-blocks/cta-hero') && !has_block('purdue-blocks/site-hero') && !has_block('purdue-blocks/title-hero')) { ?>
+<?php if (!is_front_page() && !has_block('purdue-blocks/cta-hero') && !has_block('purdue-blocks/site-hero') && !has_block('purdue-blocks/title-hero')) { ?>
 
-<section class="section">
-    <div class="container">
-        <div class="content">
-            <?php purdueBrand_the_title('is-1', false); ?>
+    <section class="section">
+        <div class="container">
+            <div class="content">
+                <?php
+                if (has_block('purdue-blocks/title-nav')) {
+                    $blocks = parse_blocks(get_the_content());
+                    foreach ($blocks as $block) {
+                        if ('purdue-blocks/title-nav' === $block['blockName']) {
+                            echo render_block($block);
+                            break;
+                        }
+                    }
+                }
+
+                purdueBrand_the_title('is-1', false);
+
+                ?>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
 
 <?php } ?>
 
 <?php
-the_content();
-?>
+// the_content();
+$blocks = parse_blocks(get_the_content());
+$content_markup = '';
+foreach ($blocks as $block) {
+    if ('purdue-blocks/title-nav' === $block['blockName']) {
+        continue;
+    } else {
+        $content_markup .= render_block($block);
+    }
+}
 
+// Remove wpautop filter so we do not get paragraphs for two line breaks in the content.
+$priority = has_filter('the_content', 'wpautop');
+if (false !== $priority) {
+    remove_filter('the_content', 'wpautop', $priority);
+}
+
+echo apply_filters('the_content', $content_markup);
+
+if (false !== $priority) {
+    add_filter('the_content', 'wpautop', $priority);
+}
+?>
