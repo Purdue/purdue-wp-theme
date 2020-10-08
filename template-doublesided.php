302212437ccdf782dbed803145637f6405a865b6
Zong <yanhuazong@gmail.com>
2020-10-08 10:07:17 -0400

    Fix double-sided template layout

--- template-doublesided.php
+++ template-doublesided.php
@@ -25,37 +25,39 @@
 </aside>
 
 <main id="site-content" role="main" class="main-content">
-    <section class="container">
-        <div class="columns is-multiline with-sideContent">
-            <div class="column is-two-thirds-desktop is-full-tablet is-full-mobile">
-            <?php
+    <div class="section">
+        <section class="container">
+            <div class="columns is-multiline with-sideContent">
+                <div class="column is-two-thirds-desktop is-full-tablet is-full-mobile">
+                <?php
 
-            if ( have_posts() ) {
+                if ( have_posts() ) {
 
-                while ( have_posts() ) {
-                    the_post();
+                    while ( have_posts() ) {
+                        the_post();
 
-                    get_template_part( 'template-parts/content', get_post_type() );
+                        get_template_part( 'template-parts/content', get_post_type() );
+                    }
                 }
-            }
 
-            ?>
-            </div>
-        
-            <?php if (is_active_sidebar('page-sidebar')) : ?>
-            <aside class="column is-one-quarter-desktop is-full-tablet is-full-mobile">
-                <div class="container section side-content">
-                    <?php dynamic_sidebar('page-sidebar'); ?>
+                ?>
                 </div>
-            </aside>
-            <?php endif; ?>
-        </div>
-    </section>
-	<?php if (!has_block('purdue-blocks/anchor-link-navigation')) { ?>
-		<button id="to-top" class="to-top-hidden">
-			<i class="fas fa-chevron-up" aria-hidden="true"></i>
-		</button>
-	<?php } ?>
+            
+                <?php if (is_active_sidebar('page-sidebar')) : ?>
+                <aside class="column is-one-quarter-desktop is-full-tablet is-full-mobile">
+                    <div class="side-content">
+                        <?php dynamic_sidebar('page-sidebar'); ?>
+                    </div>
+                </aside>
+                <?php endif; ?>
+            </div>
+        </section>
+        <?php if (!has_block('purdue-blocks/anchor-link-navigation')) { ?>
+            <button id="to-top" class="to-top-hidden">
+                <i class="fas fa-chevron-up" aria-hidden="true"></i>
+            </button>
+        <?php } ?>
+    </div>
 </main><!-- #site-content -->
 
 </div>
