42365f300a445396c0b2becdffda80457f2abc1a
Zong <yanhuazong@gmail.com>
2020-11-11 13:30:43 -0500

    Add template list ALM template

---
+++ alm_templates/template-post.php
@@ -0,0 +1,21 @@
+<div class="column is-one-third-desktop is-half-tablet is-full-mobile">
+	<div class="card listed-post">
+	<a href="<?php the_permalink(); ?>">
+		<?php if (has_post_thumbnail()) { ?>
+			<div class="card-image">
+				<figure class="card-bg-image image is-4by3" style="background-image:url('<?php echo wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>');">
+				</figure>
+			</div>
+		<?php } ?>
+		<div class="card-content">
+			<div class="media">
+				<div class="media-content">
+					<p class="title is-4">
+						<?php the_title(); ?>
+					</p>
+				</div>
+			</div>
+		</div>
+		</a>
+	</div>
+</div>
